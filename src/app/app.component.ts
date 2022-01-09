d22944f8554b13cfc4a21a378b6f0ffa172b2819
Aliaksei Maisenovich <a.maisenovich@gmail.com>
2022-01-09 16:13:44 +0300

    section 15.201

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
 
-const enum Subscription {
-  BASIC = 'basic',
-  ADVANCED = 'advanced',
-  PRO = 'PRO'
+enum Subscription {
+  BASIC = 'Basic',
+  ADVANCED = 'Advanced',
+  PRO = 'Pro'
+}
+
+enum FieldName {
+  EMAIL = 'email',
+  SUBSCRIPTION = 'subscription',
+  PASSWORD = 'password'
 }
 
 @Component({
@@ -12,5 +19,33 @@ const enum Subscription {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  sub
+  FieldName = FieldName
+
+  Subscription = Subscription
+
+  subscriptions = Object.values(Subscription)
+
+  @ViewChild('subscriptionForm')
+  subscriptionForm: NgForm
+
+  submittedValues: { [key: string]: string } | undefined;
+
+  showError = (fieldModel: NgModel) => {
+    return !fieldModel.valid && fieldModel.touched
+  }
+
+  onFormSubmit = () => {
+    if (!this.subscriptionForm.valid) {
+      return
+    }
+
+    this.submittedValues = this.subscriptionForm.value
+  }
+
+  resetSubmission = () => {
+    this.submittedValues = undefined
+    this.subscriptionForm.reset({
+      [FieldName.SUBSCRIPTION]: Subscription.ADVANCED
+    })
+  }
 }
